9a2cb199d647aef69a874f19a6c135d3de1afaae
noonworks <noonworks@gmail.com>
2019-09-11 16:43:31 +0900

    Change class name and auto-add controls.

---
+++ src/scripts/CarotinNovel.ts
@@ -0,0 +1,97 @@
+import { ScrollableWrapper } from './scroll/ScrollableWrapper';
+import { Loader } from './loader';
+import { SlidepadManager } from './slidepad/SlidePadManager';
+import { ScrollStateManager } from './scroll/ScrollStateManager';
+import { deleteTextNodeInRuby, setCustomRuby } from './util';
+
+export class CarotinNovel {
+  private bodyDom: HTMLBodyElement;
+  private wrapperDom: HTMLDivElement;
+  private slidepadDom: HTMLDivElement;
+  private shadowDom: HTMLDivElement;
+  private loaderDom: HTMLDivElement;
+  private bgDom: HTMLDivElement;
+
+  private wrapper: ScrollableWrapper;
+  private loader: Loader;
+  private slidepad: SlidepadManager;
+  private scrollManager: ScrollStateManager;
+
+  constructor() {
+    {
+      const body = document.querySelector('body');
+      const wrapper = document.querySelector('div.carotin_novel');
+      if (!body || !wrapper) {
+        throw new Error('Could not get elements.');
+      }
+      this.bodyDom = body;
+      this.wrapperDom = wrapper as HTMLDivElement;
+      this.wrapper = new ScrollableWrapper(this.wrapperDom);
+    }
+    {
+      let shadow = this.wrapperDom.querySelector('div.shadow');
+      if (!shadow) {
+        shadow = document.createElement('div');
+        shadow.classList.add('shadow');
+        this.wrapperDom.appendChild(shadow);
+      }
+      this.shadowDom = shadow as HTMLDivElement;
+    }
+    {
+      let slidepad = this.wrapperDom.querySelector('div.slidepad');
+      if (!slidepad) {
+        slidepad = document.createElement('div');
+        slidepad.classList.add('slidepad');
+        this.wrapperDom.appendChild(slidepad);
+      }
+      this.slidepadDom = slidepad as HTMLDivElement;
+      this.slidepad = new SlidepadManager(this.slidepadDom, this.wrapper);
+      this.slidepad.show();
+    }
+    {
+      let loader = this.wrapperDom.querySelector('div.loader');
+      if (!loader) {
+        loader = document.createElement('div');
+        loader.classList.add('loader');
+        this.wrapperDom.appendChild(loader);
+      }
+      this.loaderDom = loader as HTMLDivElement;
+      this.loader = new Loader(this.loaderDom);
+    }
+    {
+      let bg = this.bodyDom.querySelector('bg_wrapper');
+      if (!bg) {
+        bg = document.createElement('div');
+        bg.classList.add('bg_wrapper');
+        this.bodyDom.insertBefore(bg, this.wrapperDom);
+      }
+      this.bgDom = bg as HTMLDivElement;
+    }
+    this.scrollManager = new ScrollStateManager(this.wrapper);
+  }
+
+  public start(): void {
+    // start loading
+    this.loader.set('circle');
+    this.loader.show();
+    // fix dom
+    deleteTextNodeInRuby(this.wrapperDom);
+    setCustomRuby(this.bodyDom);
+    // scroll document to bookmarks
+    this.scrollManager
+      .restore()
+      .then(() => {
+        // start events
+        this.scrollManager.start();
+        // finish loading
+        this.loader.hide();
+      })
+      .catch((reason: Error) => {
+        console.log(reason);
+        // start events
+        this.scrollManager.start();
+        // finish loading
+        this.loader.hide();
+      });
+  }
+}
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,53 +1,6 @@
-import { deleteTextNodeInRuby, setCustomRuby } from './util';
-import { ScrollableWrapper } from './scroll/ScrollableWrapper';
-import { ScrollStateManager } from './scroll/ScrollStateManager';
-import { Loader } from './loader';
-import { SlidepadManager } from './slidepad/SlidePadManager';
+import { CarotinNovel } from './CarotinNovel';
 
-// initialize
-function initialize(): void {
-  const body = document.querySelector('body');
-  const wrapperDom = document.querySelector(
-    'div.content_wrapper'
-  ) as HTMLElement;
-  const loaderDom = document.querySelector('div.loader') as HTMLElement;
-  if (!body || !wrapperDom || !loaderDom) {
-    // show error
-    return;
-  }
-  const wrapper = new ScrollableWrapper(wrapperDom);
-  // start loading
-  const loader = new Loader(loaderDom);
-  loader.set('circle');
-  loader.show();
-  // fix dom
-  deleteTextNodeInRuby(wrapper.dom);
-  setCustomRuby(body);
-  // add parts
-  const slidepad = document.querySelector('div.slidepad');
-  let slidePadManager: SlidepadManager | null = null;
-  if (slidepad) {
-    slidePadManager = new SlidepadManager(slidepad as HTMLElement, wrapper);
-    slidePadManager.changeSize('middle');
-    slidePadManager.show();
-  }
-  // scroll document to bookmarks
-  const scrollStateManager = new ScrollStateManager(wrapper);
-  scrollStateManager
-    .restore()
-    .then(() => {
-      // start events
-      scrollStateManager.start();
-      // finish loading
-      loader.hide();
-    })
-    .catch((reason: Error) => {
-      console.log(reason);
-      // start events
-      scrollStateManager.start();
-      // finish loading
-      loader.hide();
-    });
-}
-
-window.addEventListener('DOMContentLoaded', initialize);
+window.addEventListener('DOMContentLoaded', () => {
+  const app = new CarotinNovel();
+  app.start();
+});
