a11a2f0083b7673fdf000868e67038b7e9013e30
noonworks <noonworks@gmail.com>
2019-09-16 23:02:30 +0900

    Move style parse functions to util.

--- src/scripts/theme/ThemeManager.ts
+++ src/scripts/theme/ThemeManager.ts
@@ -1,47 +1,5 @@
 import { Theme } from './Theme';
-
-const RulePrefix = ':root[data-style-theme=';
-function getThemeRules(): CSSStyleRule[] {
-  const stylerules: CSSStyleRule[] = [];
-  for (let i = 0; i < document.styleSheets.length; i++) {
-    if (document.styleSheets[i].type.indexOf('css') < 0) {
-      break;
-    }
-    const css = document.styleSheets[i] as CSSStyleSheet;
-    try {
-      const rules = css.rules;
-      for (let j = 0; j < rules.length; j++) {
-        const rule = rules[j] as CSSStyleRule;
-        if (rule.selectorText && rule.selectorText.indexOf(RulePrefix) >= 0) {
-          stylerules.push(rule);
-        }
-      }
-    } catch (e) {
-      break;
-    }
-  }
-  return stylerules;
-}
-
-const doublequoted = /^"(.*)"$/;
-const singlequoted = /^'(.*)'$/;
-function fixVal(val: string): string {
-  return val.replace(doublequoted, '$1').replace(singlequoted, '$1');
-}
-
-const prefix = /^--carotinnovel--/;
-function fixKey(key: string): string {
-  return key.replace(prefix, '');
-}
-
-function getMap(style: CSSStyleDeclaration): { [key: string]: string } {
-  const r: { [key: string]: string } = {};
-  for (let i = 0; i < style.length; i++) {
-    const key = style[i];
-    r[fixKey(key)] = fixVal(style.getPropertyValue(key));
-  }
-  return r;
-}
+import { getCSSStyleRules, getCSSRuleKeyValue } from '../util';
 
 function createTheme(map: { [key: string]: string }): Theme {
   return new Theme({
@@ -101,9 +59,9 @@ class ThemeManager {
 
   public load(): void {
     this._themes = [];
-    const stylerules = getThemeRules();
-    for (let i = 0; i < stylerules.length; i++) {
-      const map = getMap(stylerules[i].style);
+    const rules = getCSSStyleRules('data-style-theme');
+    for (let i = 0; i < rules.length; i++) {
+      const map = getCSSRuleKeyValue(rules[i].rules);
       this._themes.push(createTheme(map));
     }
     for (let i = 0; i < this._themes.length; i++) {
